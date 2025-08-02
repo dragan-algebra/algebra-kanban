d397b08f1e1f38cd7d69f1ba75dc0992aa5fc6fd
dragan-algebra <dstipa5@algebra.hr>
2025-08-02 14:12:40 +0200

    Add board-navbar

---
+++ actions/update-board/schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const UpdateBoard = z.object({
+  title: z
+    .string()
+    .nonempty({ message: "Title is required" })
+    .min(3, { message: "Title is too short" }),
+  id: z.string(),
+});
--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,45 +1,6 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-
-import { db } from "@/lib/db";
-
-// import { ListContainer } from "./_components/list-container";
-
-interface BoardIdPageProps {
-  params: {
-    boardId: string;
-  };
-}
-
-const BoardIdPage = async ({ params }: BoardIdPageProps) => {
-//   const { orgId } = await auth();
-
-//   if (!orgId) {
-//     redirect("/select-org");
-//   }
-
-//   const lists = await db.list.findMany({
-//     where: {
-//       boardId: params.boardId,
-//       board: {
-//         orgId,
-//       },
-//     },
-//     include: {
-//       cards: {
-//         orderBy: {
-//           order: "asc",
-//         },
-//       },
-//     },
-//     orderBy: {
-//       order: "asc",
-//     },
-//   });
-
+const BoardIdPage = () => {
   return (
-    <div className="p-4 h-full overflow-x-auto">
-      {/* <ListContainer boardId={params.boardId} data={lists} /> */}
+    <div>
       Board id Page
     </div>
   );
