c3fa819b80c7c7b97e13645090ecbb26009e97c9
dragan-algebra <dstipa5@algebra.hr>
2025-08-02 19:24:44 +0200

    WIP: add partial list component

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,6 +1,44 @@
-const BoardIdPage = () => {
+import { auth } from "@clerk/nextjs/server"
+import { db } from "@/lib/db"
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: Promise<{ boardId: string }>;
+};
+
+const BoardIdPage = async ({
+  params,
+}: BoardIdPageProps) => {
+  const { boardId } = await params;
+  const { orgId } = await auth();
+
+  if (!orgId) {
+    redirect("/select-team");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
   return (
-    <div>
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={boardId} data={lists} />
       Board id Page
     </div>
   );
