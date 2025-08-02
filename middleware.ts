7cb3ab4d3c4ab399ffdb6d0497533001bb9cefd1
dragan-algebra <dstipa5@algebra.hr>
2025-08-02 16:42:39 +0200

    Fix: update middleware.ts to handle routing properly

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,37 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 
-const publicRoutes = [
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-];
-
-const isPublicRoute = createRouteMatcher(publicRoutes);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook",
+  "/sign-in",
+  "/sign-up",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+  const { userId, orgId } = await auth();
+
+  if (userId && req.nextUrl.pathname === "/") {
+    if (userId) {
+      if (orgId) {
+        return NextResponse.redirect(new URL(`/team/${orgId}`, req.url));
+      } else {
+        return NextResponse.redirect(new URL("/select-team", req.url));
+      }
+    }
+    return NextResponse.next();
   }
+
+  if (!userId && !isPublicRoute(req)) {
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("returnBackUrl", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+   return NextResponse.next();
 });
+
 
 export const config = {
   matcher: [
