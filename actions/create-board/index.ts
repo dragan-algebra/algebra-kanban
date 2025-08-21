bdc0f71eb4d817809dd530e3ffa8a6deee7cc45a
dragan-algebra <dstipa5@algebra.hr>
2025-08-21 23:07:18 +0200

    Add activity component

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -7,6 +7,8 @@ import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateBoard } from "./schema";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
      const { userId, orgId } = await auth();
@@ -47,6 +49,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
                 imageUserName,
                 imageLinkHTML,
             }
+        });
+        
+        await createAuditLog({
+            entityTitle: board.title,
+            entityId: board.id,
+            entityType: ENTITY_TYPE.BOARD,
+            action: ACTION.CREATE,
         });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
